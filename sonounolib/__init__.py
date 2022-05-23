248b0e3414ec935722124c31588e6cb1243845aa
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-05-23 22:07:35 +0200

    Import streamunolib decorators and functions.

--- sonounolib/__init__.py
+++ sonounolib/__init__.py
@@ -4,12 +4,17 @@ This library provides sonification components to construct transforms.
 """
 # from importlib.metadata import version
 
+from streamunolib import exposed, hidden, media_type
+
 from .notes import asfrequency
 from .tracks import Track
 
 __all__ = [
     'Track',
     'asfrequency',
+    'exposed',
+    'hidden',
+    'media_type',
 ]
 
 # __version__ = version('sonounolib')
